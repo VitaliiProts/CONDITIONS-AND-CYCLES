646c9f219ee1350961e7e8abdf84b6871d8dcca9
Vitalii Prots <vitalikprocj@gmail.com>
2017-10-11 15:32:24 +0300

    add cycles

--- src/app.js
+++ src/app.js
@@ -34,14 +34,78 @@ if (lemonChicken) {
 		что-то иное, по-прежнему печатайте «Привет, незнакомец!».
 		------------------------------------------------------------------------*/
 
-var name = "Andrew";
+		var name = "Andrew";
 
-if(name === "Vitalii") {
-	console.log("Привет мне!");
-} else if (name === "Andrew") {
-	console.log("Привет, папа!");
-} else if(name === "Victoria") {
-	console.log("Привет, мама!");
-} else {
-	console.log("Привет, незнакомец!");
+		if(name === "Vitalii") {
+			console.log("Привет мне!");
+		} else if (name === "Andrew") {
+			console.log("Привет, папа!");
+		} else if(name === "Victoria") {
+			console.log("Привет, мама!");
+		} else {
+			console.log("Привет, незнакомец!");
+		}
+
+// Циклы
+
+// Цикл while
+
+// while(condition) {
+// 	console.log("Делаем что-то");
+// 	i++;
+// }
+
+var sheepCounted = 0;
+
+while(sheepCounted < 10) {
+	console.log("Підраховано овець: " + sheepCounted + "!");
+	sheepCounted++;
+}
+console.log("Хрррррррррр-псссс");
+
+// Цикл for
+for(var sheepCounted = 0; sheepCounted < 10; sheepCounted++) {
+	console.log("Підраховано овець: " + sheepCounted + "!");
+}
+console.log("Хрррррррррр-псссс");
+
+var timesToSayHello = 3;
+for(var i = 0; i < timesToSayHello; i++) {
+	console.log("Hello");
+}
+
+// Цикл for, массивы и строки
+var animals = ["лев", "фламинго", "белый медведь", "удав"];
+for(var i = 0; i < animals.length; i++) {
+	console.log(animals[i]);
+}
+
+var name = "Nick";
+for(var i = 0; i < name.length; i++) {
+	console.log("В моем имени есть буква " + name[i] + ".");
+}
+
+// Другие варианты применения for
+for(var x = 2; x < 10000; x *= 2) {
+	console.log(x);
+}
+
+/*
+	ПОПРОБУЙТЕ !
+		Напишите цикл for, который печатает степени тройки, не превышающие
+		10 000 (программа должна выводить 3, 9, 27 и т. д.)
+		Перепишите это задание, вместо for использовав цикл while.
+		(Подсказка: установите начальное значение перед входом в цикл.)
+*/
+
+// for
+for(var a = 3; a < 10000; a *= 3) {
+	console.log(a)
+}
+
+// while
+var b = 3;
+while(b < 10000) {
+	console.log(b);
+	b *= 3;
 }
